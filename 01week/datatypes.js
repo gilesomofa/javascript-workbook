f5d71883b12375a1b3cf17e1eb96cc93f55a53d6
Michelle Kassel <MichelleKassel@VermontsFinest.attlocal.net>
2019-08-13 15:00:44 -0500

    I'm attempting to push from VSC instead of Terminal which is good, I think.

---
+++ 01week/datatypes.js
@@ -0,0 +1,66 @@
+var d = new Date(2019,08, 13);
+
+
+function displayDate(){
+  new Date();
+  console.log ("maybe you are getting this after all?")
+}
+displayDate();
+
+let num = 13;
+let n = num.toString();
+function convertNoToString(){
+  console.log ("I am a string now");
+}
+
+convertNoToString();
+
+var text = '5';
+var pointNum = parseFloat(text);
+function stringToNumber(){
+  console.log ("string to number");
+}
+stringToNumber();
+
+typeof true
+typeof null
+typeof object
+typeof 47
+typeof NaN
+typeof "Dinh"
+
+var sumOfTwoNumbers = (num1, num2) => {
+    return num1 + num2;
+  }
+  sumOfTwoNumbers(60, 150);
+
+  let x = 7
+let y = 8
+function myDoubleConfirmation()
+{if ( x < y );
+if (x > 3);
+console.log("I am a coder");
+}
+myDoubleConfirmation();
+
+let x = 5;
+let y = 5;
+function myOneOfTwo(){
+if ( x==y );
+if ( x > 6 );
+console.log("you are getting it")
+}
+myOneOfTwo();
+
+let x=5;
+  console.log("this is variable x which is 5")
+  let y=5;
+    console.log("this is variable y which is 5 as well")
+  
+   
+ function myDoubleFalse(){
+  if ( x > y );
+  if ( x < y );
+  console.log("numbers are equal therefore no go")
+ } 
+ myDoubleFalse();
