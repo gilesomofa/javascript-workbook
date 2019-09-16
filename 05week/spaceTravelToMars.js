fb5a114d15761092684a4534c71ff04d44a75fe6
Your Name <you@example.com>
2019-09-16 15:51:08 -0500

    loops project

--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -10,7 +10,38 @@ let jobTypes = {
 };
 
 // Your code here
+class CrewMember{
+  constructor(name, job, specialSkill, ship){
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = ship;
+}
 
+enterShip(shipName){
+  this.ship = shipName;
+  shipName.crew.push(this)
+
+}
+}
+class Ship{
+  constructor(name, type, ability, crew){
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = crew;
+  }
+}
+
+missionStatement = function(){
+  let crewMember1;
+  let crewMember2;
+  let mav;
+  let hermes;
+  if( CrewMember ===
+  let 
+  return true
+}
 //tests
 if (typeof describe === 'function'){
   describe('CrewMember', function(){
