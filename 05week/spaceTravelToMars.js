0256095338b692a9b05fc37e2c57f2d9e91522f2
Your Name <you@example.com>
2019-09-16 19:13:18 -0500

    mars

--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -10,6 +10,36 @@ let jobTypes = {
 };
 
 // Your code here
+class CrewMember{
+  constructor(name, job, specialSkill, ship){
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = ship;
+}
+
+enterShip(shipName){
+  this.ship = shipName;
+  shipName.crew.push(this)
+
+}
+}
+class Ship{
+  constructor(name, type, ability, crew){
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = crew;
+  }
+}
+
+missionStatement(shipAbility, shipName);{
+  this.ship = shipName;
+  shipName.crew.push(this);
+  this.ability = shipAbility;
+return true;
+}
+
 
 //tests
 if (typeof describe === 'function'){
@@ -57,3 +87,5 @@ if (typeof describe === 'function'){
     });
   });
 }
+
+
